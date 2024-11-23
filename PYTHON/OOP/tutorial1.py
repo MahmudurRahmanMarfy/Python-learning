0e2edb9ec272d7ce963fcd05b73bfe23ef2a60ef
MahmudurRahmanMarfy <kamlavabla@gmail.com>
2024-11-24 00:19:14 +0600

    finished

--- PYTHON/OOP/tutorial1.py
+++ PYTHON/OOP/tutorial1.py
@@ -1,6 +1,7 @@
 class Employee:
 
     raise_amount = 1.04
+    num_of_emps = 0
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -8,17 +9,30 @@ class Employee:
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
     
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
-        print(emp_2.pay)
 
 emp_1 = Employee('Mahmudur', 'rahman' , 60000)
 emp_2 = Employee('Harvy','the destroyer', 75000)
+emp_3 = Employee('Nadim','vokochoko', 70000)
 
 #print(emp_1.email) 
 #print('{} {}'.format(emp_2.first, emp_2.last))
 #print('{} {}'.format(emp_1.first, emp_1.last))
-#print(emp_2.pay)
+
+emp_1.raise_amount = 2
+emp_1.apply_raise()
+print(emp_1.raise_amount)
+print(emp_1.pay)
+
+print(emp_2.pay)
+emp_2.apply_raise()
+print(emp_2.raise_amount)
+print(emp_2.pay)
+
+print(Employee.num_of_emps)
