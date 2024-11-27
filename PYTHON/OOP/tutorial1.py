8a8e864ffc79fd4347dcd6008a2d8e949c6ae4d0
MahmudurRahmanMarfy <kamlavabla@gmail.com>
2024-11-27 23:45:46 +0600

    finished

--- PYTHON/OOP/tutorial1.py
+++ PYTHON/OOP/tutorial1.py
@@ -17,6 +17,22 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def seperate(cls , emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+    
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
 emp_1 = Employee('Mahmudur', 'rahman' , 60000)
 emp_2 = Employee('Harvy','the destroyer', 75000)
 emp_3 = Employee('Nadim','vokochoko', 70000)
@@ -25,14 +41,30 @@ emp_3 = Employee('Nadim','vokochoko', 70000)
 #print('{} {}'.format(emp_2.first, emp_2.last))
 #print('{} {}'.format(emp_1.first, emp_1.last))
 
-emp_1.raise_amount = 2
-emp_1.apply_raise()
-print(emp_1.raise_amount)
-print(emp_1.pay)
+#emp_1.raise_amount = 2
+#emp_1.apply_raise()
+#print(emp_1.raise_amount)
+#print(emp_1.pay)
+#
+#print(emp_2.pay)
+#emp_2.apply_raise()
+#print(emp_2.raise_amount)
+#print(emp_2.pay)
+#
+#print(Employee.num_of_emps)
 
-print(emp_2.pay)
-emp_2.apply_raise()
-print(emp_2.raise_amount)
-print(emp_2.pay)
+#Employee.set_raise_amount(2)
+#emp_2.apply_raise()
+#emp_1.apply_raise()
+#print(emp_1.pay)
+#print(emp_2.pay)
 
-print(Employee.num_of_emps)
+emp_str_1 = 'Nadim-hossain-50000'
+first, last, pay = emp_str_1.split('-')
+
+new_employee_1 = Employee.seperate(emp_str_1)
+print(new_employee_1.pay)
+
+import datetime
+my_date = datetime.date(2016, 5, 22)
+print(Employee.is_workday(my_date))
