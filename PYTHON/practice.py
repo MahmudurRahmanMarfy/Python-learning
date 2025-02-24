d4558cd7b235faec8e2e2865faef9e8fd54865f3
MahmudurRahmanMarfy <kamlavabla@gmail.com>
2025-02-24 22:21:47 +0600

    Solved

--- PYTHON/practice.py
+++ PYTHON/practice.py
@@ -1,7 +1,12 @@
+
 def sum_numbers(text: str) -> int:
     # your code here
-    return 
+    result = []
+    for words in text.split():
+        if words.isdigit():
+            result.append(int(words))
+    return sum(result)
 
 
 print("Example:")
-print(sum_numbers("hi"))
+print(sum_numbers( "This picture is an oil on canvas painting by Danish artist Anna Petersen between 1845 and 155 years." )) 
