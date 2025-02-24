d6063c6bad6991701344ff7eea0d753ccda1925f
MahmudurRahmanMarfy <kamlavabla@gmail.com>
2025-02-24 21:57:14 +0600

    Solved

--- PYTHON/practice.py
+++ PYTHON/practice.py
@@ -1,8 +1,16 @@
-def name(text:str, start:str, end:str) -> str:
-    return text.index(start)
+def max_digit(value: int) -> int:
+    # your code here
+    return int(max(str(value)))
+    
 
-print(name("Mahmudur Rahman","M","R"))
+print("Example:")
+print(max_digit(986))
 
-#name = "Marfy"
-#print(name.index("M"))
-#print(name.index("y"))
+# These "asserts" are used for self-checking
+assert max_digit(0) == 0
+assert max_digit(52) == 5
+assert max_digit(634) == 6
+assert max_digit(1) == 1
+assert max_digit(10000) == 1
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
