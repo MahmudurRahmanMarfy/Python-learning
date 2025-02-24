4d07c83445ddf477207bc114340b07d5e5516751
MahmudurRahmanMarfy <kamlavabla@gmail.com>
2025-02-24 20:24:14 +0600

    Solved

--- PYTHON/practice.py
+++ PYTHON/practice.py
@@ -1,27 +1,8 @@
-#fatyellowrectangle = Rectangle(20, 5 , 'yellow')
-#fatyellowrectangle.height = 5 
-#fatyellowrectangle.width = 20
-#fatyellowrectangle.color = 'yellow'
-#
-#fatyellowrectangle.drawRectangle#
+def name(text:str, start:str, end:str) -> str:
+    return text.index(start)
 
-#C1 = Circle(4, "yellow")
-#
-#class Circle (Object):
-#    def__init__(self,radius,color):
-#        self.radius = 4;
-#        self.color = yellow;
+print(name("Mahmudur Rahman","M","R"))
 
-
-class Points(object):
-  def __init__(self,x,y):
-
-    self.x=x
-    self.y=y
-
-  def print_point(self):
-
-    print('x=',self.x,' y=',self.y)
-
-p1=Points(1,2)
-p1.print_point()
+#name = "Marfy"
+#print(name.index("M"))
+#print(name.index("y"))
