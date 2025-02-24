16a6bb2aca827c3075512faa0f931192afaff0e3
MahmudurRahmanMarfy <kamlavabla@gmail.com>
2025-02-24 22:09:05 +0600

    Solved

--- PYTHON/practice.py
+++ PYTHON/practice.py
@@ -1,16 +1,7 @@
-def max_digit(value: int) -> int:
+def sum_numbers(text: str) -> int:
     # your code here
-    return int(max(str(value)))
-    
+    return 
+
 
 print("Example:")
-print(max_digit(986))
-
-# These "asserts" are used for self-checking
-assert max_digit(0) == 0
-assert max_digit(52) == 5
-assert max_digit(634) == 6
-assert max_digit(1) == 1
-assert max_digit(10000) == 1
-
-print("The mission is done! Click 'Check Solution' to earn rewards!")
+print(sum_numbers("hi"))
